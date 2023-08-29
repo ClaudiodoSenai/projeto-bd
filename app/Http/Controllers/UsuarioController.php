eda4c3c94d933fdd49dd7b5a1177aae2304a3f79
ClaudiodoSenai <claudiorobertojunior5@gmail.com>
2023-08-29 15:19:53 -0300

    aula 29/08

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -9,18 +9,70 @@ use Illuminate\Support\Facades\Hash;
 
 class UsuarioController extends Controller
 {
-    public function store(UsuarioRequest $request){
+    public function store(UsuarioRequest $request)
+    {
         $usuario = Usuario::create([
-            'nome' => $request ->nome,
-            'cpf' => $request ->cpf,
-            'celular' => $request ->celular,
-            'email' => $request ->email,
+            'nome' => $request->nome,
+            'cpf' => $request->cpf,
+            'celular' => $request->celular,
+            'email' => $request->email,
             'password' => Hash::make($request->password)
-        ]); 
+        ]);
         return response()->json([
-            "success" =>true,
+            "success" => true,
             "message" => "Registred User",
-            "data"=> $usuario
-        ],200);
+            "data" => $usuario
+        ], 200);
+    }
+
+    public function pesquisarPorId($id)
+    {
+        $usuario = Usuario::find($id);
+        if ($usuario == null) {
+            return response()->json([
+                'status' => false,
+                'message' => "Usuario não encontrado"
+            ]);
+        }
+        return response()->json([
+            'status' => true,
+            'data' => $usuario
+        ]);
+    }
+
+    public function pesquisarPorCpf($cpf)
+    {
+        $usuario = Usuario::where('cpf', '=', $cpf)->first();
+        return response()->json([
+            'status' => true,
+            'data' => $usuario
+        ]);
+    }
+
+    public function retornarTodos()
+    {
+        $usuarios = Usuario::all();
+        return response()->json([
+            'status' => true,
+            'data' => $usuarios
+        ]);
+    }
+
+    public function pesquisarPorNome(Request $request)
+    {
+        $usuarios =  Usuario::where('nome', 'like', '%' . $request->nome . '%')->get();
+
+        if (count($usuarios) > 0) {
+
+            return response()->json([
+                'status' => true,
+                'data' => $usuarios
+            ]);
+        }
+
+        return response()->json([
+            'status' => false,
+            'message' => 'Não há resultados para a pesquisa.'
+        ]);
     }
 }
